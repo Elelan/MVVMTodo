bb373444f467a49ae520f766e19c50c3c9a12a08
ElelanV <vickneshelelan@gmail.com>
2021-08-01 02:45:38 +0530

    connected stateflow

---
+++ app/src/main/java/com/codinginflow/mvvmtodo/di/AppModule.kt
@@ -0,0 +1,42 @@
+package com.codinginflow.mvvmtodo.di
+
+import android.app.Application
+import androidx.room.Room
+import com.codinginflow.mvvmtodo.data.TaskDatabase
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ApplicationComponent
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.SupervisorJob
+import javax.inject.Qualifier
+import javax.inject.Singleton
+
+@Module
+@InstallIn(ApplicationComponent::class)
+object AppModule {
+
+    @Singleton
+    @Provides
+    fun providesDatabase(
+        app: Application,
+        callback: TaskDatabase.Callback
+    ) = Room.databaseBuilder(app, TaskDatabase::class.java, "task_database")
+        .fallbackToDestructiveMigration()
+        .addCallback(callback)
+        .build()
+
+    @Provides
+    fun providesTaskDao(
+        db: TaskDatabase
+    ) = db.taskDao()
+
+    @ApplicationScope
+    @Singleton
+    @Provides
+    fun providesApplicationScope() = CoroutineScope(SupervisorJob())
+}
+
+@Retention(AnnotationRetention.RUNTIME)
+@Qualifier
+annotation class ApplicationScope
